65bd57f9608c2b8ad0a2aa80cbda2842de4e7884
mihirh19 <miheerhadvani990@gmail.com>
2024-01-30 23:39:05 +0530

    Add Custom Fonts

--- cashcraft/src/App.jsx
+++ cashcraft/src/App.jsx
@@ -5,7 +5,20 @@ import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login'
+import { useEffect } from 'react'
+import WebFont from 'webfontloader';
+
+
+
 function App() {
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['JetBrains Mono']
+      }
+    })
+  }, []);
 
   return (
     <Fragment>
