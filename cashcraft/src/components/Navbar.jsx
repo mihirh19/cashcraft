2bae38011875eef7174f4fbc0865ff9e11c30e4f
mihirh19 <miheerhadvani990@gmail.com>
2024-02-01 22:28:46 +0530

    login page designed

--- cashcraft/src/components/Navbar.jsx
+++ cashcraft/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import styles from "../App.module.scss"
-
+import { Link } from "react-router-dom"
 const Navbar = () => {
    return (
       <nav className={styles.navbar}>
          <div className={styles.navbar_logo}>CashCraft</div>
          <ul className={styles.navbar_links}>
+            <Link to={"/login"}>
+               <li className={styles.navbar_link}>login</li>
+            </Link>
             <li className={styles.navbar_link}>Dashboard</li>
             <li className={styles.navbar_link}>Expenses</li>
             <li className={styles.navbar_link}>Friends</li>
