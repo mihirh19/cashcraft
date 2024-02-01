1c1b7a50fe39cfc11f6a113959081862224e7b75
mihirh19 <miheerhadvani990@gmail.com>
2024-02-01 22:20:47 +0530

    Login Component designed

--- cashcraft/src/components/Login.jsx
+++ cashcraft/src/components/Login.jsx
@@ -1,13 +1,47 @@
 import { Fragment } from "react"
 import styles from "./styles/Login.module.scss"
+import { useState } from "react";
 const Login = () => {
+   const [username, setUsername] = useState('');
+   const [password, setPassword] = useState('');
+
+   const handleLogin = (e) => {
+      e.preventDefault();
+      // Add your login logic here
+      alert('Logging in with:   ' + username);
+   };
+
    return (
       <Fragment>
-         <div className={styles.bg_facts}>
-            <div className={styles.container}>
-               
+         <div className={styles.login_all}>
+            <div className={styles.login_container}>
+               <h2>Login</h2>
+               <form onSubmit={handleLogin}>
+                  <label htmlFor="username">Username:</label>
+                  <input
+                     type="text"
+                     id="username"
+                     value={username}
+
+                     onChange={(e) => setUsername(e.target.value)}
+                  />
+
+                  <label htmlFor="password">Password:</label>
+                  <input
+                     type="password"
+                     id="password"
+                     value={password}
+
+                     onChange={(e) => setPassword(e.target.value)}
+                  />
+
+                  <button type="submit">
+                     Login
+                  </button>
+               </form>
             </div>
          </div>
+
       </Fragment>
    )
 }
