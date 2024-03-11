29c9ba500cfb68a5d2c11e60ad3af937995656f7
mihirh19 <miheerhadvani990@gmail.com>
2024-03-11 23:08:47 +0530

    Dockerize the App

--- cashcraft/vite.config.js
+++ cashcraft/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
+    watch: {
+      usePolling: true
+    },
+  },
 })
